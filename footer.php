af5c31093adf47e4de666a82a26f075bc7a4a887
hassanjavedd2006 <115046981+hassanjavedd2006@users.noreply.github.com>
2023-09-15 19:56:48 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,54 @@
+<style>
+    footer {
+        background: linear-gradient(180deg, #0a2044 0%, #273752 100%);
+    }
+
+    footer {
+        margin-top: 10vh;
+    }
+  .link{
+color: white;
+ }
+ .link:hover{
+    color: grey;
+ }
+</style>
+
+<footer class=" text-white py-4 ">
+    <div class="container">
+        <div class=" mb-5" style="margin-left: 10vh;">
+            <h1 class="text-primary">CROSSBORDERS🛫🛫</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <h3 style="top:0px">Branding</h3>
+                <ul class="list-unstyled text-light">
+                    <li><a href="#" class="link">Home</a></li>
+                    <li><a href="#" class="link">About</a></li>
+                    <li><a href="#" class="link">Services</a></li>
+                </ul>
+                <div class="social-links">
+                    <a href="#"><img src="assets/img/facebook.png" alt="Facebook"></a>
+                    <a href="#"><img src="assets/img/twitter.png" alt="Twitter"></a>
+                    <a href="#"><img src="assets/img/instagram.png" alt="Instagram"></a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <h3 style="top:0px">Column 2</h3>
+                <ul class="list-unstyled">
+                    <li><a class="link" href="../crossborders/2admin/register.php?tbl=universities">Register as UNIVERSITY</a></li>
+                    <li><a class="link" href="../crossborders/2admin/register.php?tbl=companies">Register as COMPANY/EMPLOYER</a></li>
+                    <li><a class="link" href="#">Link 3</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h3 style="top:0px">Column 3</h3>
+                <ul class="list-unstyled">
+                    <li><a href="#">Link A</li></a>
+                    <li><a href="#">Link B</li></a>
+                    <li><a href="#">Link C</li></a>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
